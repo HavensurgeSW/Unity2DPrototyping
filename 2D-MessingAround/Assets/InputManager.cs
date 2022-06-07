7cf70050f1ed80d7775928b0779a98578994637f
Matias <matias@karplus.com.ar>
2022-06-07 10:40:00 -0300

    Added menu scene, not yet functional

---
+++ 2D-MessingAround/Assets/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    bool pauseStatus;
+
+    private void Start()
+    {
+        pauseStatus = false;
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (pauseStatus)
+            {
+                pauseCanvas.SetActive(false);
+                pauseStatus = false;
+            }
+            else
+            {
+                pauseCanvas.SetActive(true);
+                pauseStatus = true;
+            }
+    }
+}
